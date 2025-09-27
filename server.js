3790b2c3e5d05bcc1840e1bab9850175883dd0a5
Abel Andres <abel03_toluca@hotmail.com>
2025-09-26 23:55:21 -0600

    sefunciona

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3008;
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Rutas de la API
 app.use('/api', userRoutes);
